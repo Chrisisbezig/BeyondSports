3d158c0be36e6e0a439993824efedd80c5986211
chrisisbezig <chris.schouten@hva.nl>
2025-06-27 13:56:43 +0200

    Added Playermanager the manager to handle all players. Time: 0h45m

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    // Singleton instance
+    public static PlayerManager instance;
+    private List<PlayerInstance> playerInstances = new List<PlayerInstance>();
+
+    [SerializeField] private GameObject playerPrefab;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void InitializePlayers(JsonFormat jsonData)
+    {
+        foreach (var player in jsonData.Players)
+        {
+            CreatePlayer(player);
+        }
+    }
+
+    private void CreatePlayer(Player player)
+    {
+        float yrotation = 0;
+        if (player.MovementOrientation != null)
+        {
+            yrotation = player.MovementOrientation;
+        }
+
+        Quaternion rotation = Quaternion.Euler(0, player.MovementOrientation, 0);
+        GameObject playerObject = Instantiate(playerPrefab, player.Position, rotation);
+    }
+}
+
+public class PlayerInstance
+{
+    public int Id;
+    public GameObject PlayerObject;
+    public Vector3 Position;
+}
